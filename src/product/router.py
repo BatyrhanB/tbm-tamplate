d199ecd6ddfe52f52a6320ef430567f0eb2503c4
Батырхан <baatyr3030@gmail.com>
2023-07-17 21:04:11 +0600

    schemas added for product and category

--- src/product/router.py
+++ src/product/router.py
@@ -9,12 +9,12 @@ from src.product import models, schemas
 router = APIRouter(prefix="/product", tags=["product"])
 
 
-@router.get("/")
+@router.get("/", response_model=schemas.ProductBase)
 async def get_products(
     offset: int = 0,
     limit: int = Query(default=100, lte=100),
     session: AsyncSession = Depends(get_async_session),
-):
+) -> list[schemas.ProductBase]:
     sql = select(models.Product).order_by(models.Product.id).limit(limit).offset(offset)
     db_products = (await session.execute(sql)).scalars().unique().all()
     return db_products
@@ -30,8 +30,10 @@ async def add_specific_product(
     return {"status": "success"}
 
 
-@router.get("/{product_id}")
-async def get_product(product_id: int, session : AsyncSession = Depends(get_async_session)):
+@router.get("/{product_id}", response_model=schemas.ProductBase)
+async def get_product(
+    product_id: int, session: AsyncSession = Depends(get_async_session)
+) -> list[schemas.ProductBase]:
     sql = select(models.Product).where(models.Product.id == product_id)
     db_product = (await session.execute(sql)).scalars().all()
     if not db_product:
--- src/product/schemas.py
+++ src/product/schemas.py
@@ -1,16 +1,50 @@
 from datetime import datetime
-
+from typing import List, Optional
 from pydantic import BaseModel
 
 
-class ProductCreate(BaseModel):
+class CategoryBase(BaseModel):
+    title: str
+    description: str
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class Category(CategoryBase):
     id: int
-    title : str
-    description : str
-    category_id: int 
-    type : str
-    origin : str
-    certification : str
+
+    class Config:
+        orm_mode = True
+
+
+class ProductBase(BaseModel):
+    title: str
+    description: str
+    category_id: int
+    type: str
+    origin: str
+    certification: str
     color: str
-    price : float
+    price: float
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class Product(ProductBase):
+    id: int
     created_at: datetime
+    category: Category
+
+    class Config:
+        orm_mode = True
+
+
+class CategoryWithProducts(Category):
+    products: List[Product] = []
+
+    class Config:
+        orm_mode = True
